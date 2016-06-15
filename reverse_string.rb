780aee6fa5a57a24a3dc82434f2b6be7f4498bae
Courtney Buras <cburas2311@gmail.com>
2016-06-15 15:41:55 -0500

    Setup recursive method syntax

--- reverse_string.rb
+++ reverse_string.rb
@@ -1,9 +1,13 @@
-def reverse_string(string)
+def reverse_string_iterate(string)
 	new_string = ""
 	string_arr = string.chars
 	while string_arr.length > 0
 		new_string << string_arr.delete_at(-1)
 	end
 	new_string
+end
+
+def reverse_string_recursive(string)
+	
 end
 

6b6fe366c92f5022192e6effd4bb47331e35f1e4
Courtney Buras <cburas2311@gmail.com>
2016-06-15 15:40:50 -0500

    Add recursive tests

--- spec/reverse_string_spec.rb
+++ spec/reverse_string_spec.rb
@@ -1,12 +1,25 @@
 require_relative '../reverse_string'
 
-describe 'reverse_string' do 
-	it 'reverses a given word' do
-		expect(reverse_string("comma")).to eq "ammoc"
+describe 'reversing a string' do
+
+	context 'reverse_string_iterate' do
+		it 'reverses a given word by iterating' do
+			expect(reverse_string_iterate("comma")).to eq "ammoc"
+		end
+
+		it 'reverses a given sentence by iterating' do
+			expect(reverse_string_iterate("It's all greek to me.")).to eq ".em ot keerg lla s'tI"
+		end
 	end
 
-	# it 'reverses a given sentence' do
-	# 	expect(reverse_string("It's all greek to me.")).to eq ".em ot keerg lla s'ti"
-	# end
-	
+	context 'reverse_string_recursive' do
+		it 'reverses a given word through recursion' do
+			expect(reverse_string_recursive("comma")).to eq "ammoc"
+		end
+
+		# it 'reverses a given sentence through recursion' do
+		# 	expect(reverse_string_recursion("It's all greek to me.")).to eq ".em ot keerg lla s'tI"
+		# end
+	end
 end
+

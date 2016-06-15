da3f1a1868c9c41812d044119420f2e752b873f6
Courtney Buras <cburas2311@gmail.com>
2016-06-15 16:27:26 -0500

    Write recursive method (Thanks Elliegit add reverse_string.rb)

--- reverse_string.rb
+++ reverse_string.rb
@@ -8,6 +8,11 @@ def reverse_string_iterate(string)
 end
 
 def reverse_string_recursive(string)
-	
+	return string if string.length <= 1
+		string[-1] + reverse_string_recursive(string[0..-2])
+
 end
 
+# what is my base case?
+# when do I stop iterating/recursing?
+	# when -1 is now 0 index wise?

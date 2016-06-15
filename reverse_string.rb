e50e80fba39ec2097f81c7f667d98cdc219837f7
Courtney Buras <cburas2311@gmail.com>
2016-06-15 15:27:16 -0500

    Iterative method working

--- reverse_string.rb
+++ reverse_string.rb
@@ -1,16 +1,9 @@
 def reverse_string(string)
-	
+	new_string = ""
+	string_arr = string.chars
+	while string_arr.length > 0
+		new_string << string_arr.delete_at(-1)
+	end
+	new_string
 end
 
-# take string apart by character and insert each into array using '.chars'
-#   OOORRRRRR
-# move second to last letter in front of string, then add final letter of original to beginning of new string. (sounds like '.length' for the end)
-# new_string = ""
-# 	string_copy = string.dup
-# 	i = 0
-# 	while i < 11
-# 		move = string.slice(-2)
-# 		new_string.insert(0, move)
-# 		i += 1
-# 	end
-# 	new_string.insert(0, string[-1])

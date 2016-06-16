fad3c52a97705c478c77123232dc72c94e7b602e
Courtney Buras <cburas2311@gmail.com>
2016-06-16 14:30:50 -0500

    Add recursive tests

--- spec/palindrome_checker_spec.rb
+++ spec/palindrome_checker_spec.rb
@@ -1,11 +1,19 @@
 require_relative '../palindrome_checker'
 
 describe 'palindrome_checker' do
-	it 'gives true if palindrome' do
-		expect(palindrome_checker("abba")).to eq true
+	it 'gives true if palindrome is found through iteration' do
+		expect(palindrome_checker_iterative("abba")).to eq true
 	end
 
-	# it 'gives false if not a palindrome' do
-	# 	expect(palindrome_checker("atom")).to eq false
-	# end
+	it 'gives false if not a palindrome is found through iteration' do
+		expect(palindrome_checker_iterative("atom")).to eq false
+	end
+
+	it 'gives true if palindrome is found through recursion' do
+		expect(palindrome_checker_recursive("abba")).to eq true
+	end
+
+	it 'gives false if not a palindrome is found through recusion' do
+		expect(palindrome_checker_recursive("atom")).to eq false
+	end
 end
